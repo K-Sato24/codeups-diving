eb87f1c9f713f72990730c8f39c0af7e34e707dc
Sato <katsu.sato24@gmail.com>
2024-05-17 17:04:02 +0900

    2024_0518_1704

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,16 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+  // ハンバーガー
+  $(function () {
+    $(".js-hamburger,.js-drawer").click(function () {
+      $(".js-hamburger").toggleClass("is-active");
+      $(".js-header").toggleClass("is-active");
+      $(".js-drawer").fadeToggle();
+      // $("body").toggleClass("is-fixed");
+    });
+
+  });
+
 
 });
