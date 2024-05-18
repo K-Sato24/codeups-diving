6419b8e22b36b832384740926737612bdd9f9c18
Sato <katsu.sato24@gmail.com>
2024-05-18 20:43:09 +0900

    2024_0518_2043

--- src/js/script.js
+++ src/js/script.js
@@ -9,8 +9,30 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
       $(".js-drawer").fadeToggle();
       // $("body").toggleClass("is-fixed");
     });
-
   });
 
+});
 
+// fvスライダー
+var swiper = new Swiper(".js-fv-swiper", {
+  loop: true,
+  effect: "fade",
+  speed: 600,
+  autoplay: {
+    delay: 6000,
+    disableOnInteraction: false,
+  },
+});
+
+
+// Campaignスライダー
+var swiper = new Swiper(".js-campaign-swiper", {
+  slidesPerView: "auto",
+  spaceBetween: 24,
+  // centeredSlides: true,
+  allowTouchMove: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
