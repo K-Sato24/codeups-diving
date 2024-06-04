84cb82e19c683d222cdc3c8b6fda79dd1ad1eb62
Sato <katsu.sato24@gmail.com>
2024-06-04 23:03:06 +0900

    2024_0604_2303

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -4,44 +4,49 @@ const tl = gsap.timeline({
     onComplete: initSwiper
   });
 
-tl.fromTo('.js-loading',{
+tl.from('.js-opening',{
+  autoAlpha: 1
+},'opening').fromTo('.js-loading',{
   autoAlpha: 0
 },{
-  duration:2.0,
+  duration:0.4,
   autoAlpha: 1
-}).fromTo('.js-loading',{
+},'opening').fromTo('.js-loading',{
   autoAlpha: 1
 },{
   duration:1.0,
   autoAlpha: 0
-}).fromTo('.js-fv-img-01',{
+},).fromTo('.js-opening01',{
   y:'100%',
   opacity:0
 },{
   y:0,
-  duration:2.0,
+  duration:1.0,
   opacity:1,
-  delay:0.3,
+  delay:0.2,
   ease: "circ.out"
-
-},'img').fromTo('.js-fv-img-02',{
+},'img').fromTo('.js-opening02',{
   y:'100%',
   opacity:0
 },{
   y:0,
-  duration:2.0,
+  duration:1.0,
   opacity:1,
-  delay:0.8,
+  delay:0.5,
   ease: "circ.out"
 
 },'img').fromTo('.js-fv-title',{
   opacity:0
 },{
-  duration:2.5,
+  duration:0.7,
   opacity:1,
-}).fromTo('.js-header',{
+  delay:0.2
+},'ending').fromTo('.js-header',{
   opacity:0
 },{
-  duration:2.0,
+  duration:0.7,
   opacity:1,
-});
+  delay:0.2
+},'ending').to('.js-opening',{
+  autoAlpha: 0
+},'ending');
